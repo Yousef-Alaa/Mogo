e233123431fe30a2850286cd0f9e0f135734c3a2
Yousef <yousefalaa11223344@gmail.com>
2020-08-23 03:32:32 +0200

    Add The Project Files

---
+++ js/plugins.js
@@ -0,0 +1,24 @@
+/*global $*/
+
+$(function () {
+    
+    'use strict';
+    
+    $('.fit-head-1').fitText(1.1, {
+        
+        maxFontSize: "35px"
+        
+    });
+});
+
+$(function () {
+    
+    'use strict';
+    
+    $('.fit-head-2').fitText(1.1, {
+        
+        maxFontSize: "80px"
+        
+    });
+});
+
---
+++ js/script.js
@@ -0,0 +1,120 @@
+/*global document, window, setTimeout*/
+
+var snake = document.getElementById("snake"),
+    
+    menuHidden = document.getElementById('menu-hid'),
+    
+    menuShow = document.getElementById('menu-show'),
+    
+    settingDiv = document.getElementById("setting-div"),
+    
+    settingBtn1 = document.getElementById("setting-btn-1"),
+    
+    settingBtn2 = document.getElementById("setting-btn-2"),
+    
+    darkMode = document.getElementById("dark"),
+    
+    themeConatainer = document.getElementById('ptc'),
+    
+    themeBtnDef = document.getElementById('theme-defualt'),
+    
+    themeBtnBlue = document.getElementById('theme-blue'),
+    
+    themeBtnPink = document.getElementById('theme-pink'),
+    
+    themeBtnYellow = document.getElementById('theme-yellow'),
+    
+    topButton = document.getElementById("top-button");
+
+/* Loading Function */
+function load() {
+    
+    'use strict';
+    
+    snake.style.display = "none";
+}
+
+window.onload = setTimeout(load, 3000);
+
+/* Responsive Menu Function */
+menuShow.onclick = function () {
+    'use strict';
+    document.querySelectorAll('ul')[0].style.right = "0";
+};
+
+menuHidden.onclick = function () {
+    'use strict';
+    document.querySelectorAll('ul')[0].style.right = "-100%";
+};
+
+/* Show Setting Bar */
+settingBtn1.onclick = function () {
+    
+    'use strict';
+    
+    settingBtn2.style.left = "200px";
+    
+    settingDiv.style.left = "0";
+    
+};
+
+/* Hidden Setting Bar */
+settingBtn2.onclick = function () {
+    
+    'use strict';
+        
+    settingBtn2.style.left = "0";
+    settingDiv.style.left = "-200px";
+};
+
+/* On/Off Dark Mode */
+darkMode.onclick = function () {
+    
+    'use strict';
+    
+    document.body.classList.toggle("dark-mode");
+};
+
+/* To Top Button Function */
+window.onscroll = function () {
+    'use strict';
+    
+    if (window.pageYOffset >= 600) {
+        topButton.style.bottom = "25px";
+    } else {
+        topButton.style.bottom = "-25px";
+    }
+};
+
+topButton.onclick = function () {
+    'use strict';
+    window.scrollTo(0, 0);
+};
+
+/* Change Theme Color */
+themeBtnDef.onclick = function () {
+    'use strict';
+    
+    themeConatainer.classList.remove('theme-blue', 'theme-pink', 'theme-yellow');
+};
+
+themeBtnBlue.onclick = function () {
+    'use strict';
+    
+    themeConatainer.classList.remove('theme-pink', 'theme-yellow');
+    themeConatainer.classList.add('theme-blue');
+};
+
+themeBtnPink.onclick = function () {
+    'use strict';
+    
+    themeConatainer.classList.remove('theme-blue', 'theme-yellow');
+    themeConatainer.classList.add('theme-pink');
+};
+
+themeBtnYellow.onclick = function () {
+    'use strict';
+    
+    themeConatainer.classList.remove('theme-blue', 'theme-pink');
+    themeConatainer.classList.add('theme-yellow');
+};
